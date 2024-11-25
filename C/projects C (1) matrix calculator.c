5cd68fd5ab0e99f0c862146ef190d51e8a3060e6
AlexxLJ <alexcreate2000@gmail.com>
2024-11-25 18:13:35 +0100

    Add files : (1)matrix calculator + (2)Contact list

---
+++ C/projects C (1) matrix calculator.c
@@ -0,0 +1,409 @@
+
+#include <stdio.h>
+#include <stdlib.h> 
+#include <time.h> 
+#define N 100
+
+typedef struct{
+	int Matr1;
+	int Matr2;
+	int Result;
+	
+}matrix;
+
+matrix M1[5];
+
+void empty(); 
+void start (int b);
+void introduce (int a, int b);
+void random (int a, int b, int matriz[a][b]);		
+void dictation (int a, int b, int matriz[a][b]);	
+void read (int a, int b, int matriz[a][b]);	
+void Add(int a, int b);
+void Subtract(int a, int b);	
+void Multiply(int a, int b, int matriz[a][b]);
+void Determinant(int a, int b, int matriz[a][b]);
+
+
+int main(){
+	
+	int b=0;
+	
+	empty();
+	start(b);
+	
+	
+	return 0;
+	
+}
+	
+	
+void empty() {
+	
+	int i;
+		
+	for (i = 0; i<N; i++){
+		M1[i].Matr1 = 0;
+		M1[i].Matr2 = 0;
+		M1[i].Result = 0;
+		
+	}
+}
+
+
+void start (int b){
+	
+	int op, d1, d2,D;
+	
+	
+	do{
+		if (M1[0].Matr1 == 0 || M1[1].Matr1 == 0){
+			
+			printf ("We are going to represent a matrix. \n");
+			D=0;
+		
+		}
+		else{
+		
+			printf ("\nWe are going to represent a matrix. \n");
+				
+		}
+		
+		printf ("Choose between: \n");
+		printf ("(1)Square matrix. \n");
+		printf ("(2)Rectangular matrix. \n");
+		
+		scanf("%i", &op);
+		
+	}while (op < 1 || op > 2);
+	
+	printf ("\n");
+	
+	switch(op){
+		
+	case 1:
+		d1=2;
+		
+		do{
+			
+			printf ("Enter your dimension in d1 format: \n");
+			fflush(stdin);
+			scanf("%i", &d1);
+			
+			
+		}while (d1 < 2 || d1 > 9);
+		
+		if (D == 0) {
+			D = d1;
+			b = D;
+			//c=a;}
+		}
+		else {
+			
+			D=b;
+			do{
+				printf ("Enter your dimension in d1 format: \n");
+				fflush(stdin);
+				scanf("%i", &d1);
+				
+				
+			}while (d1 != D);	
+		}
+		
+		introduce(d1,d1);	
+		
+		
+		break;
+	case 2:
+		
+		if (D == 0){
+			d1=2;
+			d2=3;
+			
+			do{printf ("Enter your dimension in d1xd2 format: \n");
+				fflush(stdin);
+				scanf("%ix%i", &d1, &d2);
+				printf("\n");
+				
+			}while(d1 ==d2);		
+			
+			D=d2;
+			b = D;
+			//c=a;
+		}else {
+			D = b;
+			
+			do{
+				printf ("Enter your dimension in d1xd2 format: \n");
+				fflush(stdin);
+				scanf("%ix%i", &d1, &d2);
+			
+			
+				if (d1 != D){
+					printf ("IMPOSSIBLE with that dimension \n");
+					printf ("Try again if you want.\n");
+				}
+			
+			}while (d1 != D);	}
+		
+		introduce(d1,d2);	
+		
+		break;
+		
+		}
+}	
+
+	
+	
+void introduce (int a, int b){	
+	
+	int op1;
+
+	int matriz1[a][b];
+	
+	do{
+		
+		printf ("Choose between: \n");
+		printf ("(1)A random matrix. \n");
+		printf ("(2)A dictated matrix number by number. \n");
+		fflush(stdin);
+		scanf("%i", &op1);
+		
+	}while (op1 < 1 || op1 > 2);
+	
+	printf ("\n");	
+	
+	switch(op1){
+	case 1:
+		
+			random(a, b, matriz1);
+			
+	
+		break;
+	case 2:
+			
+			dictation(a, b, matriz1);;
+	
+			
+			break;
+			
+	}
+
+}
+
+void random(int a, int b, int matriz[a][b]){
+		
+	int i,j;
+	
+	srand(time(NULL));
+	
+	for (i = 0; i<a; i++){
+		for (j = 0; j < b; j++){
+			matriz[i][j]=rand()%10;
+		}
+	}
+	read (a, b, matriz);
+		
+}	
+		
+	
+void dictation (int a, int b, int matriz[a][b]){
+			
+	int i,j, x;
+			
+	for (i = 0; i<a; i++){
+		for (j = 0; j < b; j++){
+			printf ("Type row by row, digit by digit: ");
+			fflush(stdin);
+			scanf("%i", &x);
+			matriz[i][j]= x;
+		}
+	}
+	
+	printf ("\n");
+	read (a, b, matriz);
+			
+}	
+	
+
+	
+void read (int a, int b, int matriz[a][b]){
+		
+	int i,j, y=0, d=10;
+	M1[90].Result = b;	
+		
+		
+	for (i = 0; i<a; i++){
+		for (j = 0; j < b; j++){
+			printf ("%i ", matriz[i][j]);
+			
+			if ((M1[0].Matr1 == 0 && M1[1].Matr1 == 0) || d == 0 ){
+				M1[y].Matr1=matriz[i][j];	
+				d=0;
+			}else{
+				M1[y].Matr2=matriz[i][j];	
+			}
+			y++;	
+			
+		} 
+			
+		printf ("\n");
+		
+	}
+		
+	int op2 ;
+		
+	
+	if ( d == 0){
+			
+			do{
+				
+				printf ("\nWhat operation you want to do with it? \n");
+				printf ("Choose between: \n");
+				printf ("(1)Addition. \n");
+				printf ("(2)Subtraction \n");
+				printf ("(3)Multiplication. \n");
+				printf ("(4)Calculate its determinant. \n");
+				fflush(stdin);
+				scanf("%i",&op2);
+				
+				
+			}while(op2 < 1 || op2 > 4);
+			
+			
+				printf ("\n");
+			
+			
+			switch(op2){
+			case 1:
+				Add(a,b);
+				break;
+			case 2:
+				Subtract(a,b);
+				break;
+			case 3:
+				Multiply(a,b, matriz);
+				break;
+			case 4:
+				Determinant(a,b, matriz);
+				break;
+			}	
+	}
+}
+		
+
+		void Add(int a, int b){
+			
+			introduce (a,b);
+			
+			
+			int i, j, y=0;
+			printf ("\n");
+			printf ("This is the result: \n");
+			
+			for (i = 0; i<a; i++){
+				for (j = 0; j < b; j++){
+					printf ("%i ", M1[y].Matr1+M1[y].Matr2);
+					y++;
+				} 
+				
+				printf ("\n");
+			}
+			
+		}
+
+			
+		void Subtract(int a, int b){
+			
+			introduce(a,b);
+			
+			printf ("\n");
+			printf ("This is the result: \n");	
+			
+			int i, j, y=0;
+			
+			
+			for (i = 0; i<a; i++){
+				for (j = 0; j < b; j++){
+					printf ("%i ", M1[y].Matr1-M1[y].Matr2);
+					y++;
+				} 
+				
+				printf ("\n");
+			}	
+		}	
+
+			
+		void Multiply(int a, int b, int matriz[a][b]){
+			
+			
+			int i, j, k, y=0, z=0, x1;
+
+			start(b);
+			
+			x1 = M1[90].Result ;	
+			
+			for (y = 0; y<a*x1; y++){
+				M1[y].Result = 1; 
+			}
+			y=0;
+			
+			printf ("\nThis is the result: \n");
+			for (i = 0; i<a; i++){
+				for (j = 0; j<x1; j++){
+					for (k = 0; k < b; k++) {	
+							if (M1[z].Result == 1){
+								
+								
+								y = y + M1[j+k*x1].Matr2*matriz[i][k];
+								
+							}
+						
+						
+					}
+						
+						M1[z].Result = y;
+					
+						printf ("%i ", M1[z].Result);
+						z++;
+						y=0;
+						
+				}
+				printf("\n");
+			}
+			
+
+		}
+			
+
+		void Determinant(int a, int b, int matriz[a][b]){
+				
+			
+			if (a == b && a < 4){
+				printf ("\n\n");
+			
+				printf("It is possible and its determinant is: ");
+				
+				if ( a == 2 ){
+					
+					printf("%i", matriz[0][0]*matriz[1][1]-matriz[0][1]*matriz[1][0]);
+					
+				}else 
+					{
+					
+					printf("%i", matriz[0][0]*matriz[1][1]*matriz[2][2]+matriz[1][0]*matriz[2][1]*matriz[0][2]+matriz[0][1]*matriz[1][2]*matriz[2][0]-matriz[0][2]*matriz[1][1]*matriz[2][0]-matriz[0][1]*matriz[1][0]*matriz[2][2]-matriz[0][0]*matriz[2][1]*matriz[1][2]);
+				}
+			}	
+						
+			else{
+				
+				printf("Not possible if not square.\n");
+				printf("If so, , ");
+				printf("at the moment I cannot calculate with matrices whose d1>3");
+			}
+				
+		
+		}
+					
+					
+					
---
+++ C/projects C (2) Contact list.c
@@ -0,0 +1,253 @@
+
+#include<stdio.h>
+#include<string.h>
+#define N 50
+
+typedef struct{
+	char name[N];
+	int number;
+	int added;
+	char COMMENTs[N];
+}contact;
+
+typedef struct{
+	char name_owner[N];
+	contact known[10];
+	int full_bi;
+}list;
+
+list bib[3]; 
+
+void empty();
+void change(char palabra[]);
+void addlist();
+void addcont();
+void consult();	
+
+
+int main(){
+	int op;
+	char key;
+	int c=0;
+	
+	empty();
+	
+	do{
+		do{
+			printf("Choose between: \n");
+			printf("(1) Add a new list. \n");
+			
+			if(c>0){
+			printf("(2) Add a new contact to a list. \n");}
+			if(c==2){
+			printf("(3) Consult a contact. \n");}
+			scanf("%i", &op);
+			
+		} while(op < 1 || op > 3 || (c == 0 && op > 1));
+		
+		switch(op){
+			case 1:{
+					c=1;
+					addlist();
+				break;
+		}
+			case 2:{
+				if(c>0){
+					c=2;
+					addcont();}
+				
+				break;
+		}
+			case 3:{
+				if(c==2){
+					
+					consult();}
+				
+				break;		
+		}
+	}
+		printf("Wanna do something else? (S/N)");
+		fflush(stdin);
+		scanf("%c", &key);
+		
+	} while(key == 's' || key == 'S' );
+	
+	return 0;
+}
+	
+	
+void empty() {
+	int i,j;
+	
+	for (i = 0; i<3; i++){
+		bib[i].full_bi = 0;
+		for (j = 0; j<10; j++){
+			bib[i].known[j].added = 0;
+		}
+	}
+}
+
+
+
+void change(char word[N]){
+	int i;
+		
+	for (i = 0; i < N; i++){
+		if (word[i] == '\n'){
+			word[i] = '\0';
+		}
+	}
+}
+
+	
+void addlist(){
+	int i, aux;
+	aux = 0;
+		
+	for (i = 0; i < 3 && aux ==0; i++){
+		if (bib[i].full_bi == 0){
+			printf("Enter the name for the owner of the contact list: ");
+			fflush(stdin);
+			fgets(bib[i].name_owner, N, stdin);
+			change(bib[i].name_owner);
+			
+			bib[i].full_bi = 1;
+			aux = 1;
+		}
+	}
+	if (aux == 0){
+		printf("There is no free space left for a new contact. \n");
+	}
+}
+
+	
+void addcont(){
+	int i, op, aux = 0;
+	
+		
+	for (i = 0; i < 3; i++){
+		if (bib[i].full_bi == 1){
+			printf("(%i) %s.\n", i, bib[i].name_owner);
+			
+		}
+	}
+	scanf ("%i", &op);
+	
+	for (i = 0; i < 10 && aux == 0; i++){
+		if (bib[op].known[i].added == 0){
+			printf("Enter the name of the contact: ");
+			fflush(stdin);
+			fgets(bib[op].known[i].name, N, stdin);
+			change(bib[op].known[i].name);
+			
+			printf("Enter the number of the contact: ");
+			fflush(stdin);
+			scanf("%i", &bib[op].known[i].number);
+		
+			do{printf("Do you want to enter a comment? CLICK : \n");
+			printf("(1) = \"Yes\"\n");
+			printf("(2) = \"No\"\n");
+			fflush(stdin);
+			scanf("%i", &op);
+			}while (op > 2 || op < 1);
+			
+			if (op == 1){
+				
+			
+			fflush(stdin);
+			fgets(bib[op].known[i].COMMENTs, N, stdin);
+			change(bib[op].known[i].COMMENTs);
+			}
+			
+			bib[op].known[i].added = 1;
+			aux = 1;
+		}
+	}
+}
+	
+
+
+void consult(){
+	
+	int op, i, j, aux, numero;
+	char search[N];
+	
+	do{
+		printf ("How do you want to search: \n");
+		printf ("(1) Contact name. \n");
+		printf ("(2) Contact number. \n");
+		printf ("(3) Contact comment. \n");
+		scanf ("%i", &op);
+	
+	}while (op<1 || op > 3);
+	
+	switch(op){
+	case 1:{
+		printf("Enter the name of the contact: ");
+		fflush(stdin);
+		fgets(search, N, stdin);
+		change(search);
+		
+		for(i = 0; i<3; i++){
+			aux =1;
+			for(j = 0; j<10; j++){
+				aux = strcmp(search,bib[i].known[j].name);
+				if (aux == 0){
+					printf ("The contact %s is stored in the %s list. \n", search, bib[i].name_owner);
+					printf("The contact number is: %i. \n", bib[i].known[j].number);
+					printf("Contact COMMENT is: %s \n", bib[i].known[j].COMMENTs);
+				}	
+			}
+		}	
+	
+		
+		break;
+	}
+	
+	case 2:{
+			printf("Enter the number of the contact: ");
+			fflush(stdin);
+			scanf("%i", &numero);
+			
+			for(i = 0; i<3; i++){
+				
+				for(j = 0; j<10; j++){
+					if(numero == bib[i].known[j].number){
+						
+						   printf ("El número de teléfono es de: %s. \n", bib[i].known[j].name);
+						   printf ("El contacto %s se encuentra guardado en la lista de %s. \n", bib[i].known[j].name , bib[i].name_owner);
+						   printf("El COMENTARIO del contacto es: %s. \n", bib[i].known[j].COMMENTs);
+						   
+					   }	
+					}	
+				}
+				
+			
+			
+			break;
+		}
+			
+	case 3:{
+				printf("Enter the COMMENT (or nickname): ");
+				fflush(stdin);
+				fgets(search, N, stdin);
+				change(search);
+				
+				for(i = 0; i<3; i++){
+					aux = 1;
+					for(j = 0; j<10; j++){
+						aux = strcmp(search,bib[i].known[j].COMMENTs);
+						if (aux == 0){
+							printf ("El contacto cuyo apodo es %s se llama %s. \n", search, bib[i].name_owner);
+							printf ("Su número de teléfono es: %i. \n", bib[i].known[j].number);
+							printf ("El contacto %s se encuentra guardado en la lista de %s. \n", search, bib[i].name_owner);
+							
+						}	
+					}
+				}	
+				
+				
+				break;
+			}		
+	}
+}
